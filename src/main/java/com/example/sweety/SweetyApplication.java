5a1f42d23aeea5a4507ea3f8ab33f9ca1eb7716e
psso83 <pkse11283@g.hongik.ac.kr>
2026-01-07 17:38:12 +0900

    commit first

---
+++ src/main/java/com/example/sweety/SweetyApplication.java
@@ -0,0 +1,13 @@
+package com.example.sweety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweetyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sweety/SweetyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sweety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
